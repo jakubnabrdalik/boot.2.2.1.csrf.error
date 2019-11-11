ecdeee848e85bf7048ef46808de5c86bcf4c75b6
Jakub Nabrdalik <jakubn@gmail.com>
2019-11-11 17:28:26 +0100

    static import

--- src/test/groovy/eu/solidcraft/hentairental/HentaiRentalApplicationTests.groovy
+++ src/test/groovy/eu/solidcraft/hentairental/HentaiRentalApplicationTests.groovy
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.servlet.MockMvc
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
